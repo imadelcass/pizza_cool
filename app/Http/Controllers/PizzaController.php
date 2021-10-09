a6d73c3becf6decaf357ac537088f5ba9b8fb69d
imad elcass <imadelcass@gmail.com>
2021-10-09 23:13:35 +0100

    first commit

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Pizza;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
 
 class PizzaController extends Controller
